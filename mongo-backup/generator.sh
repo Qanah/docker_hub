9d3076abd86c60c59ebbef379abcc5564911d8b9
Qanah <i.qanah@gmail.com>
2024-04-22 20:44:58 +0300

    add mongo backup v7.0

--- mongo-backup/generator.sh
+++ mongo-backup/generator.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+trap 'catch $? $LINENO' EXIT
+catch() {
+ echo "catching!"
+ if [ "$1" != "0" ]; then
+   # error handling goes here
+   echo "Error $1 occurred on $2"
+ fi
+}
 
 echo "Running docker-compose build"
 docker-compose build --no-cache
