52c74be3d7302f400df61516513154745e0ce871
qanah <i.qanah@gmail.com>
2024-04-23 09:13:57 +0300

    try to fix mongo backup

--- mongo-backup/generator.sh
+++ mongo-backup/generator.sh
@@ -10,9 +10,9 @@ catch() {
 }
 
 echo "Running docker-compose build"
-docker-compose build --no-cache
+docker compose build --no-cache
 echo "Finished docker-compose build"
 
 echo "Running docker-compose push"
-docker-compose push
+docker compose push
 echo "Finished docker-compose push"
